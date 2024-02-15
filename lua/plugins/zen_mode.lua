9074a4f96832329f0769c58bc9f6dbf19f9f27f9
hopezh <hope.zh@gmail.com>
2024-02-15 23:04:42 +0800

    add zen_mode

---
+++ lua/plugins/zen_mode.lua
@@ -0,0 +1,26 @@
+return {
+    "folke/zen-mode.nvim",
+
+    keys = {
+        {
+            "<leader>zz",
+            "<cmd>ZenMode<cr>",
+            desc = "Zen Mode",
+        },
+    },
+
+    config = function()
+        local zen = require("zen-mode")
+
+        zen.setup({
+            window = {
+                backdrop = 0.9,
+                width = 100,
+                options = {
+                    -- signcolumn = "no",
+                    -- number = false,
+                },
+            },
+        })
+    end,
+}
