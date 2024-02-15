241fd25b910f4e058d48bdd10037e3204fa182ae
hopezh <hope.zh@gmail.com>
2024-02-15 22:51:38 +0800

    edit symbols_outline config

---
+++ lua/plugins/symbols_outline.lua
@@ -0,0 +1,11 @@
+return {
+    "simrat39/symbols-outline.nvim",
+
+    keys = {
+        {
+            "<leader>sy",
+            "<cmd>SymbolsOutline<cr>",
+            desc = "Symbol Outline",
+        },
+    },
+}
