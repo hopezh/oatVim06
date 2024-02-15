f74693f36fbe1e7a950a5248ecd5b54c45cc2682
hopezh <hope.zh@gmail.com>
2024-02-15 22:42:39 +0800

    add cellular

---
+++ lua/plugins/cellular.lua
@@ -0,0 +1,17 @@
+return {
+    "eandrju/cellular-automaton.nvim",
+
+    -- keys = {
+    -- {
+    --     "<leader>zm",
+    --     "<cmd>CellularAutomaton make_it_rain<cr>",
+    --     desc = "Make it rain...",
+    -- },
+
+    -- {
+    --     "<leader>zg",
+    --     "<cmd>CellularAutomaton game_of_life<cr>",
+    --     desc = "Game of life...",
+    -- },
+    -- },
+}
