32e9143e59738df9358ff74132f0ab86d8b8d568
hopezh <hope.zh@gmail.com>
2024-02-15 22:05:03 +0800

    edit notify config

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "rcarriga/nvim-notify",
+
+        opts = {
+            -- when using transparent in tokyonight, need to set a bg color here
+            background_colour = "#000000",
+        },
+
+        -- the above is equivalent to the following:
+        -- require("notify").setup({
+        --     -- when using transparent in tokyonight, need to set a bg color here
+        --     background_colour = "#000000",
+        -- }),
+    },
+}
