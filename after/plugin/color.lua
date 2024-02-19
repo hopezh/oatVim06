1bbe5bf647a5523ab611065c4fb5580589a82254
hopezh <hope.zh@gmail.com>
2024-02-19 15:28:22 +0800

    update color

--- after/plugin/color.lua
+++ after/plugin/color.lua
@@ -24,12 +24,12 @@ vim.keymap.set(
 
 -- change bg color of cursolrline, cursorline number, & cursorcolumn -----------
 -- vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "red" })
--- vim.api.nvim_set_hl(0, "CursorLine", { bg = "red" })
 -- vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "#292e42" }) -- tkyonight default
+-- vim.api.nvim_set_hl(1, "CursorLineNr", { bg = "#272727" })
+-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "red" })
 -- vim.api.nvim_set_hl(0, "CursorLine", { bg = "#292e42" }) -- tkyonight default
-vim.api.nvim_set_hl(1, "CursorLineNr", { bg = "#272727" })
-vim.api.nvim_set_hl(2, "CursorLine", { bg = "#272727" })
-vim.api.nvim_set_hl(3, "CursorColumn", { bg = "#252525" })
+-- vim.api.nvim_set_hl(2, "CursorLine", { bg = "#272727" })
+-- vim.api.nvim_set_hl(3, "CursorColumn", { bg = "#252525" })
 
 -- change the color of ColorColumn ---------------------------------------------
 -- !!! must disable virt-column
