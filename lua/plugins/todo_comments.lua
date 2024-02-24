af4ea18c1457465daaccd2489b730cefa8f8f21c
hopezh <hope.zh@gmail.com>
2024-02-24 15:00:23 +0800

    add todo_comments

---
+++ lua/plugins/todo_comments.lua
@@ -0,0 +1,30 @@
+return {
+    "folke/todo-comments.nvim",
+
+    keys = {
+        -- {
+        --     "<leader>xO",
+        --     "<cmd>TodoLocList keywords=oat<cr>",
+        --     desc = "show oat in this project",
+        -- },
+        {
+            "<leader>zo",
+            "<cmd>TodoTrouble keywords=oat<cr>",
+            desc = "show oat in Trouble",
+        },
+        {
+            "<leader>zl",
+            "<cmd>TodoTelescope keywords=oat<cr>",
+            desc = "show oat in Telescope",
+        },
+    },
+
+    -- add custom comment keyword ----------------------------------------------
+    opts = {
+        merge_keywords = true,
+        keywords = {
+            -- oat = { icon = "", color = "#505050" },
+            o = { icon = "", color = "#505050" },
+        },
+    },
+}
