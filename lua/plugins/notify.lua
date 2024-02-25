f9988cc0d0f0caa40a8b6441c7fa72f18006b89e
hopezh <hope.zh@gmail.com>
2024-02-25 13:28:20 +0800

    move message window to lower right

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -5,6 +5,9 @@ return {
         opts = {
             -- when using transparent in tokyonight, need to set a bg color here
             background_colour = "#000000",
+
+            -- move message window to lower right corner
+            top_down = false,
         },
 
         -- the above is equivalent to the following:
