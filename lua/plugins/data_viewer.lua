3c59e74c9dcb6f4c6d9e09693770a8a5403f1dd5
hopezh <hope.zh@gmail.com>
2024-02-25 13:10:01 +0800

    add data_viewer

---
+++ lua/plugins/data_viewer.lua
@@ -0,0 +1,10 @@
+return {
+    "vidocqh/data-viewer.nvim",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "kkharji/sqlite.lua", -- Optional, sqlite support
+    },
+
+    opts = {},
+}
