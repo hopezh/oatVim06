4f0d0e8114013f25e59f380065e7116a96e590cd
hopezh <hope.zh@gmail.com>
2024-04-18 16:19:28 +0800

    edit neo-tree

--- lua/plugins/neo_tree.lua
+++ lua/plugins/neo_tree.lua
@@ -19,16 +19,17 @@ return {
 
         default_component_configs = {
             indent = {
+                with_markers = true, -- default
                 -- last_indent_marker = "└", -- default
                 last_indent_marker = "╰",
                 -- expander_collapsed = "", -- default
-                -- expander_expanded = "",  -- default
+                -- expander_expanded = "", -- default
                 expander_collapsed = "",
                 expander_expanded = "",
             },
             icon = {
-                folder_closed = "",
-                folder_open = "",
+                folder_closed = "", -- default
+                folder_open = "", -- default
                 -- folder_closed = "",
                 -- folder_open = "",
                 folder_empty = "󰜌",
