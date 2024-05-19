d36ee1dfb0e2370d54b22d8183823892b1359e9e
hopezh <hope.zh@gmail.com>
2024-05-19 23:39:46 +0800

    clean todo-comments

--- lua/plugins/todo_comments.lua
+++ lua/plugins/todo_comments.lua
@@ -23,7 +23,6 @@ return {
     opts = {
         merge_keywords = true,
         keywords = {
-            -- oat = { icon = "", color = "#505050" },
             o = { icon = "", color = "#505050" },
         },
     },
