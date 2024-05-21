f13a5888185335d8ef36df44fbf6f60492d06abc
hopezh <hope.zh@gmail.com>
2024-05-21 21:30:00 +0800

    add a few colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+    },
+
+    { "ellisonleao/gruvbox.nvim" },
+
+    { "rebelot/kanagawa.nvim" },
+
+    { "EdenEast/nightfox.nvim" },
+
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            -- colorscheme = "gruvbox",
+            -- colorscheme = "catppuccin",
+            -- colorscheme = "kanagawa-dragon",
+            colorscheme = "nightfox",
+        },
+    },
+}
