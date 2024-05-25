480abf45289c7378ab815efb785377cb6b3bf1b2
hopezh <hope.zh@gmail.com>
2024-05-25 16:57:48 +0800

    add neofusion color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,20 +4,28 @@ return {
     --     name = "catppuccin",
     --     priority = 1000,
     -- },
-    --
+
     -- { "ellisonleao/gruvbox.nvim" },
-    --
+
     -- { "rebelot/kanagawa.nvim" },
     --
     -- { "EdenEast/nightfox.nvim" },
-    --
-    -- {
-    --     "LazyVim/LazyVim",
-    --     opts = {
-    --         -- colorscheme = "gruvbox",
-    --         -- colorscheme = "catppuccin",
-    --         -- colorscheme = "kanagawa-dragon",
-    --         -- colorscheme = "nightfox",
-    --     },
-    -- },
+
+    {
+        "diegoulloao/neofusion.nvim",
+        priority = 1000,
+        config = true,
+        opts = {},
+    },
+
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            -- colorscheme = "gruvbox",
+            -- colorscheme = "catppuccin",
+            -- colorscheme = "kanagawa-dragon",
+            -- colorscheme = "nightfox",
+            -- colorscheme = "neofusion",
+        },
+    },
 }
