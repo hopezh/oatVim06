69e8918e1fa7cd7b46589671ac7f763569d33b29
hopezh <hope.zh@gmail.com>
2024-05-26 10:42:14 +0800

    update lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,22 +1,35 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable",
+      lazypath,
+  })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
     spec = {
-        -- add LazyVim and import its plugins
+        -- add LazyVim and import its plugins ----------------------------------
         { "LazyVim/LazyVim", import = "lazyvim.plugins" },
         -- import any extras modules here
-        -- { import = "lazyvim.plugins.extras.lang.typescript" },
+        { import = "lazyvim.plugins.extras.coding.mini-surround" },
+        { import = "lazyvim.plugins.extras.lang.markdown" },
+        { import = "lazyvim.plugins.extras.lang.python" },
+        { import = "lazyvim.plugins.extras.lang.typescript" },
         -- { import = "lazyvim.plugins.extras.lang.json" },
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-        -- import/override with your plugins
+
+        -- import/override with your plugins -----------------------------------
         { import = "plugins" },
     },
+
     defaults = {
         -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
         -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
@@ -26,13 +39,17 @@ require("lazy").setup({
         version = false, -- always use the latest git commit
         -- version = "*", -- try installing the latest stable version for plugins that support semver
     },
+
     dev = {
         path = "~/projects",
         patterns = { "jmbuhr", "quarto-dev" },
         fallback = true,
     },
+
     install = { colorscheme = { "tokyonight", "habamax" } },
+
     checker = { enabled = true }, -- automatically check for plugin updates
+
     performance = {
         rtp = {
             -- disable some rtp plugins
