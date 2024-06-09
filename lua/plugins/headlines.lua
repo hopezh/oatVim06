27189e5cab090db92fd12f235bf6f5a1b1f240af
hopezh <hope.zh@gmail.com>
2024-06-10 00:32:20 +0800

    minor

--- lua/plugins/headlines.lua
+++ lua/plugins/headlines.lua
@@ -25,8 +25,8 @@ return {
                     [[
                         (thematic_break) @dash
                         (fenced_code_block) @codeblock
-                        (block_quote_marker) @quote
                         (block_quote (paragraph (inline (block_continuation) @quote)))
+                        (block_quote_marker) @quote
                     ]]
                 ),
 
