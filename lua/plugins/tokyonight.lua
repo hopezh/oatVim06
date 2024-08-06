3efc0a9f184ae3d5e93d57376e55c2e333cf8d43
hopezh <hope.zh@gmail.com>
2024-08-06 20:41:51 +0800

    disable cursorline bg highlight

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -5,6 +5,7 @@ return {
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
+
         style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         -- style = "day", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
         light_style = "day", -- The theme is used when the background is set to light
@@ -53,6 +54,7 @@ return {
                 bg = colors.bg_highlight, -- extend cursorline hl bg color to line number
                 italic = true,
             }
+            highlights.CursorLine = {} -- disable cursorline highlight
         end,
     },
 }
