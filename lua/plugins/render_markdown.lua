1d25a294066f9c2763123d87ab6017adb391e48c
hopezh <hope.zh@gmail.com>
2024-08-06 20:19:54 +0800

    reactivate render-markdown.nvim

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -1,8 +1,6 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
-    enable = false,
-
-    name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+    -- enable = false,
 
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
