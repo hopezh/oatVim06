e01974c870e691de5423cde57b0662c6ca094b01
hopezh <hope.zh@gmail.com>
2024-09-09 00:06:54 +0800

    add test_bpy.py

--- test/test_bpy.py
+++ test/test_bpy.py
@@ -1,3 +1,12 @@
 import bpy
 
-bpy.ops.mesh.primitive_cube_add()
+bpy.ops.mesh.primitive_cube_add(size=4)
+
+cube_obj = bpy.context.active_object
+
+cube_location = cube_obj.location
+
+cube_location.x = 5
+cube_location.y = 5
+cube_location.z = 5
+
