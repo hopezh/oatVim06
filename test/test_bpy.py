65f0c13cd39f4e0541be422f837d4bf50056c82f
hopezh <hope.zh@gmail.com>
2024-09-14 23:45:10 +0800

    install jedi-lsp

--- test/test_bpy.py
+++ test/test_bpy.py
@@ -1,4 +1,5 @@
 import bpy
+import numpy as np
 
 bpy.ops.mesh.primitive_cube_add(size=4)
 
@@ -9,4 +10,3 @@ cube_location = cube_obj.location
 cube_location.x = 5
 cube_location.y = 5
 cube_location.z = 5
-
